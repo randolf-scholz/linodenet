979e926d16ea53cbfeaa21703891f3bfa6d69dd8
Randolf Scholz <rscholz@ismll.de>
2021-11-17 01:27:13 +0100

    added filter unit test

--- linodenet/filters/_filters.py
+++ linodenet/filters/_filters.py
@@ -55,7 +55,7 @@ class FilterABC(nn.Module):
         super().__init__()
 
     @abstractmethod
-    def forward(self, x: Tensor, y: Tensor, /) -> Tensor:
+    def forward(self, y: Tensor, x: Tensor) -> Tensor:
         """Forward pass of the filter.
 
         Parameters
@@ -64,20 +64,23 @@ class FilterABC(nn.Module):
             The current estimation of the state of the system.
         y: Tensor
             The current measurement of the system.
-        mask: Tensor, optional
-            A mask to be applied to the input tensor.
 
         Returns
         -------
         Tensor:
             The updated state of the system.
         """
-        pass
 
 
 @autojit
 class KalmanFilter(FilterABC):
     r"""Classical Kalman Filter."""
+
+    # CONSTANTS
+    input_size: Final[int]
+    """CONST: The input size."""
+    hidden_size: Final[int]
+    """CONST: The hidden size."""
 
     # PARAMETERS
     H: Tensor
@@ -89,20 +92,24 @@ class KalmanFilter(FilterABC):
     ZERO: Tensor
     r"""BUFFER: A constant value of zero."""
 
-    def __init__(self, x_dim: int, y_dim: int):
+    def __init__(self, /, input_size: int, hidden_size: int):
         super().__init__()
+
+        # CONSTANTS
+        self.input_size = input_size
+        self.hidden_size = hidden_size
 
         # BUFFERS
         self.register_buffer("ZERO", torch.zeros(1))
 
         # PARAMETERS
-        self.H = nn.Parameter(torch.empty(y_dim, x_dim))
-        self.R = nn.Parameter(torch.empty(y_dim, x_dim))
+        self.H = nn.Parameter(torch.empty(input_size, hidden_size))
+        self.R = nn.Parameter(torch.empty(input_size, hidden_size))
         nn.init.kaiming_normal_(self.H, nonlinearity="linear")
         nn.init.kaiming_normal_(self.R, nonlinearity="linear")
 
     @jit.export
-    def forward(self, x: Tensor, y: Tensor, /, *, P: Optional[Tensor] = None) -> Tensor:
+    def forward(self, /, y: Tensor, x: Tensor, *, P: Optional[Tensor] = None) -> Tensor:
         r"""Forward pass of the filter.
 
         Parameters
@@ -120,11 +127,11 @@ class KalmanFilter(FilterABC):
         mask = ~torch.isnan(y)
         H = self.H
         R = self.R
-        r = y - torch.matmul(H, x)
+        r = y - torch.einsum("ij, ...j -> ...i", H, x)
         r = torch.where(mask, r, self.ZERO)
         z = torch.linalg.solve(H @ P @ H.t() + R, r)
         z = torch.where(mask, z, self.ZERO)
-        return x + P @ H.t() @ z
+        return x + torch.einsum("ij, jk, ..k -> ...i", P, H.t(), z)
 
 
 @autojit
@@ -133,15 +140,19 @@ class KalmanCell(FilterABC):
 
     HP: dict = {
         "activation": "Identity",
-        "autoregressive": True,
+        "autoregressive": False,
     }
 
     # CONSTANTS
     autoregressive: Final[bool]
     """CONST: Whether the filter is autoregressive or not."""
+    input_size: Final[int]
+    """CONST: The input size."""
+    hidden_size: Final[int]
+    """CONST: The hidden size."""
 
     # PARAMETERS
-    H: Optional[Tensor] = None
+    H: Tensor
     r"""PARAM: the observation matrix."""
     kernel: Tensor
     r"""PARAM: The kernel matrix."""
@@ -150,7 +161,7 @@ class KalmanCell(FilterABC):
     ZERO: Tensor
     r"""BUFFER: A constant value of zero."""
 
-    def __init__(self, x_dim: int, y_dim: int, /, **HP: Any):
+    def __init__(self, /, input_size: int, hidden_size: int, **HP: Any):
         super().__init__()
 
         deep_dict_update(self.HP, HP)
@@ -158,22 +169,26 @@ class KalmanCell(FilterABC):
 
         # CONSTANTS
         self.autoregressive = HP["autoregressive"]
+        self.input_size = input_size
+        self.hidden_size = hidden_size
 
         # BUFFERS
         self.register_buffer("ZERO", torch.zeros(1))
 
         # PARAMETERS
-        self.kernel = nn.Parameter(torch.empty(y_dim, y_dim))
+        self.kernel = nn.Parameter(torch.empty(input_size, input_size))
         nn.init.kaiming_normal_(self.kernel, nonlinearity="linear")
 
         # MODULES
         self.activation = ACTIVATIONS[HP["activation"]]()
 
         if self.autoregressive:
-            assert x_dim == y_dim, "Autoregressive filter requires x_dim == y_dim"
-            self.H = None
+            assert (
+                hidden_size == input_size
+            ), "Autoregressive filter requires x_dim == y_dim"
+            self.H = torch.eye(input_size)
         else:
-            self.H = nn.Parameter(torch.empty(y_dim, x_dim))
+            self.H = nn.Parameter(torch.empty(input_size, hidden_size))
             nn.init.kaiming_normal_(self.H, nonlinearity="linear")
 
     @jit.export
@@ -190,15 +205,27 @@ class KalmanCell(FilterABC):
         """
         if self.autoregressive:
             return x
-        return self.H @ x
+        return torch.einsum("ij, ...j -> ...i", self.H, x)
 
     @jit.export
-    def forward(self, x: Tensor, y: Tensor, /) -> Tensor:
-        r"""Forward pass of the filter."""
+    def forward(self, y: Tensor, x: Tensor) -> Tensor:
+        r"""Signature: `[...,m], [...,n] ⟶ [...,n]`.
+
+        Parameters
+        ----------
+        y: Tensor
+        x: Tensor
+
+        Returns
+        -------
+        Tensor
+        """
         # create the mask
         mask = ~torch.isnan(y)
         r = torch.where(mask, y - self.h(x), self.ZERO)
-        z = torch.where(mask, self.kernel @ r, self.ZERO)
+        z = torch.where(
+            mask, torch.einsum("ij, ...j -> ...i", self.kernel, r), self.ZERO
+        )
         return x + self.activation(self.h(z))
 
 
@@ -209,6 +236,13 @@ class KalmanBlockCell(FilterABC):
         "nblocks": 5,
         "activation": "Identity",
         "autoregressive": True,
+        "Cell": {
+            "__name__": "GRUCell",
+            "input_size": None,
+            "hidden_size": None,
+            "autoregressive": True,
+            "activation": "Identity",
+        },
     }
 
     # CONSTANTS
@@ -216,8 +250,12 @@ class KalmanBlockCell(FilterABC):
     """CONST: Whether the filter is autoregressive or not."""
     nblocks: Final[int]
     """CONST: The number of blocks."""
+    input_size: Final[int]
+    """CONST: The input size."""
+    hidden_size: Final[int]
+    """CONST: The hidden size."""
 
-    def __init__(self, x_dim: int, y_dim: int, /, **HP: Any):
+    def __init__(self, /, input_size: int, hidden_size: int, **HP: Any):
         super().__init__()
 
         deep_dict_update(self.HP, HP)
@@ -226,16 +264,21 @@ class KalmanBlockCell(FilterABC):
         # CONSTANTS
         self.nblocks = HP["nblocks"]
         self.autoregressive = HP["autoregressive"]
+        self.input_size = input_size
+        self.hidden_size = hidden_size
 
         # MODULES
         self.activation = ACTIVATIONS[HP["activation"]]()
         self.blocks = nn.Sequential(
-            *(KalmanCell(x_dim, y_dim, **HP) for _ in range(self.nblocks))
+            *(
+                KalmanCell(input_size=input_size, hidden_size=hidden_size, **HP)
+                for _ in range(self.nblocks)
+            )
         )
 
     @jit.export
-    def forward(self, x: Tensor, y: Tensor) -> Tensor:
-        r"""Signature: `[...,n], [...,n] ⟶ [...,n]`.
+    def forward(self, y: Tensor, x: Tensor) -> Tensor:
+        r"""Signature: `[...,m], [...,n] ⟶ [...,n]`.
 
         Parameters
         ----------
@@ -247,7 +290,7 @@ class KalmanBlockCell(FilterABC):
         xhat: Tensor
         """
         for block in self.blocks:
-            x = block(x, y)
+            x = block(y, x)
         return x
 
 
@@ -275,7 +318,7 @@ class RecurrentCellFilter(FilterABC):
     hidden_size: Final[int]
     """CONST: The hidden size."""
 
-    def __init__(self, input_size: int, hidden_size: int, /, **HP: Any):
+    def __init__(self, /, input_size: int, hidden_size: int, **HP: Any):
         super().__init__()
 
         deep_dict_update(self.HP, HP)
--- test/test_filters.py
+++ test/test_filters.py
@@ -1,8 +1,56 @@
-r"""#TODO add module summary line.
-
-#TODO add module description.
-"""
+r"""Test if filters satisfy idempotence property."""
 
 import logging
 
+import torch
+
+from linodenet.filters import KalmanBlockCell, KalmanCell
+
 __logger__ = logging.getLogger(__name__)
+NAN = torch.tensor(float("nan"))
+
+
+def test_idempotency():
+    r"""Check whether idempotency holds."""
+    batch_dim, m, n = (3, 4, 5), 100, 100
+    x = torch.randn(*batch_dim, n)
+    y = torch.randn(*batch_dim, m)
+    mask = y > 0
+    y[mask] = NAN
+
+    # Test KalmanCel
+    model = KalmanCell(
+        input_size=n, hidden_size=m, autoregressive=True, activation="ReLU"
+    )
+    result = model(y, x)
+    assert not torch.isnan(result).any(), "Output contains NANs!"
+    __logger__.info("KalmanCell: No NaN outputs ✔ ")
+
+    # verify IDP condition
+    y[~mask] = x[~mask]
+    assert (x == model(y, x)).all(), "Idempotency failed!"
+    __logger__.info("KalmanCell: Idempotency holds ✔ ")
+
+    # Test KalmanBlockCell
+    model = KalmanBlockCell(
+        input_size=n, hidden_size=m, autoregressive=True, activation="ReLU"
+    )
+    result = model(y, x)
+    assert not torch.isnan(result).any(), "Output contains NANs!"
+    __logger__.info("KalmanBlockCell: No NaN outputs ✔ ")
+
+    # verify IDP condition
+    y[~mask] = x[~mask]
+    assert (x == model(y, x)).all(), "Idempotency failed!"
+    __logger__.info("KalmanBlockCell: Idempotency holds ✔ ")
+
+
+def __main__():
+    logging.basicConfig(level=logging.INFO)
+    __logger__.info("Testing Filters started!")
+    test_idempotency()
+    __logger__.info("Testing Filters finished!")
+
+
+if __name__ == "__main__":
+    __main__()
