1a5719b25d42c1f354710ecbe48e8ce0338e10eb
Randolf Scholz <randolf.scholz@gmail.com>
2023-05-07 21:13:57 +0200

    custom operators

---
+++ src/linodenet/models/encoders/spectral_normalization.py
@@ -0,0 +1,13 @@
+"""Re-Implementation of Spectral Normalization Layer.
+
+Notes
+-----
+The default implementation provided by torch is problematic [1]_.
+Particularly, the
+
+
+
+References
+----------
+[1] https://pytorch.org/docs/stable/_modules/torch/nn/utils/spectral_norm.html#spectral_norm
+"""
