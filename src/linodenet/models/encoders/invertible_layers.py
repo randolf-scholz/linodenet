163e3cfbce8b2e2fa68d4567ccd72f9f0c041db5
Randolf Scholz <randolf.scholz@gmail.com>
2023-05-08 08:15:33 +0200

    wip

--- src/linodenet/models/encoders/invertible_layers.py
+++ src/linodenet/models/encoders/invertible_layers.py
@@ -1,3 +1,20 @@
-"""Implementation of invertible layers."""
+"""Implementation of invertible layers.
+
+Layers:
+- Affine: $y = Ax+b$ and $x = A⁻¹(y-b)$
+    - A diagonal
+    - A triangular
+    - A tridiagonal
+- Element-wise:
+    - Monotonic
+- Shears (coupling flows): $y_A = f(x_A, x_B)$ and $y_B = x_B$
+    - Example: $y_A = x_A + e^{x_B}$ and $y_B = x_B$
+- Residual: $y = x + F(x)$
+    - Contractive: $y = F(x)$ with $‖F‖<1$
+    - Low Rank Perturbation: $y = x + ABx$
+- Continuous Time Flows: $ẋ=f(t, x)$
+
+
+"""
 
 __all__ = []
--- src/linodenet/models/encoders/spectral_normalization.py
+++ src/linodenet/models/encoders/spectral_normalization.py
@@ -3,11 +3,9 @@
 Notes
 -----
 The default implementation provided by torch is problematic [1]_.
-Particularly, the
-
-
 
 References
 ----------
 [1] https://pytorch.org/docs/stable/_modules/torch/nn/utils/spectral_norm.html#spectral_norm
 """
+# TODO: Add reasons
