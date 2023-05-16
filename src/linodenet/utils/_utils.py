a17375b18f5e5a9054feb6b87997c86da73b33f6
Randolf Scholz <randolf.scholz@gmail.com>
2023-05-17 00:37:20 +0200

    added register_cache function

--- src/linodenet/utils/_utils.py
+++ src/linodenet/utils/_utils.py
@@ -11,21 +11,24 @@ __all__ = [
     "is_dunder",
     "is_private",
     "pad",
+    "register_cache",
     # Classes
     "timer",
+    "reset_caches",
 ]
 
 import gc
 import logging
 import sys
+import warnings
 from collections.abc import Iterable, Mapping
 from contextlib import ContextDecorator
 from copy import deepcopy
 from functools import wraps
 from importlib import import_module
 from time import perf_counter_ns
-from types import ModuleType, TracebackType
-from typing import Any, ClassVar, Literal, TypeVar
+from types import MethodType, ModuleType, TracebackType
+from typing import Any, Callable, ClassVar, Literal, TypeVar
 
 import torch
 from torch import Tensor, jit, nn
@@ -204,4 +207,90 @@ class timer(ContextDecorator):
         self.LOGGER.info("Stopped timer.")
         gc.enable()
         self.LOGGER.info("Re-Enabled garbage collection.")
+        return False
+
+
+def register_cache(self: nn.Module, name: str, func: Callable[[], Tensor]) -> None:
+    """Register a cache to a module.
+
+    - creates a buffer that stores the result of func().
+    - creates a "recompute_{name}" that updates the buffer.
+    - creates a "recompute_all" that recomputes all buffers in order.
+    - creates a "cached_tensors" that returns a dictionary of all buffers.
+    """
+    if hasattr(self, name):
+        raise AttributeError(f"{self} already has attribute {name}")
+
+    if not hasattr(self, "cached_tensors"):
+        cached_tensors: dict[str, Tensor] = {}
+        self.cached_tensors = cached_tensors  # type: ignore[assignment]
+        self.__annotations__["cached_tensors"] = dict[str, Tensor]
+
+    cached_tensors = self.cached_tensors  # type: ignore[assignment]
+
+    # register the buffer
+    self.register_buffer("name", func())
+    self.__annotations__[name] = Tensor
+    cached_tensors[name] = getattr(self, name)
+
+    # register the recompute function
+    def recompute(obj: nn.Module) -> None:
+        setattr(obj, name, func())
+
+    setattr(self, f"recompute_{name}", MethodType(recompute, self))
+
+    # register the recompute all function
+    def recompute_all(obj: nn.Module) -> None:
+        for key in obj.cached_tensors:  # type: ignore[union-attr]
+            f = getattr(obj, f"recompute_{key}")
+            f()
+
+    self.recompute_all = MethodType(recompute_all, self)
+
+
+class reset_caches(ContextDecorator):
+    """Context manager for resetting caches."""
+
+    LOGGER: ClassVar[logging.Logger] = logging.getLogger(f"{__module__}/{__qualname__}")  # type: ignore[name-defined]
+
+    def __init__(
+        self,
+        module: nn.Module,
+        *,
+        on_enter: bool = True,
+        on_exit: bool = True,
+    ) -> None:
+        self.module = module
+        self.on_enter = on_enter
+        self.on_exit = on_exit
+
+        num_caches = 0
+        for layer in self.module.modules():
+            if hasattr(layer, "cached_tensors"):
+                num_caches += 1
+        self.LOGGER.info("Found %d caches.", num_caches)
+        if num_caches == 0:
+            warnings.warn("No caches found.")
+
+    def recompute_all(self) -> None:
+        """Recompute all cached tensors."""
+        for layer in self.module.modules():
+            f = getattr(layer, "recompute_all", lambda: None)
+            f()
+
+    def __enter__(self) -> Self:
+        if self.on_enter:
+            self.LOGGER.info("Resetting caches.")
+            self.recompute_all()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> Literal[False]:
+        if self.on_exit:
+            self.LOGGER.info("Resetting caches.")
+            self.module.recompute_all()  # type: ignore[operator]
         return False
