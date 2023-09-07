629fe9768a825d1c102cec65527b0f730360b66a
Randolf Scholz <randolf.scholz@gmail.com>
2023-09-07 19:30:24 +0200

    updated deps

--- bugs/tmp.py
+++ bugs/tmp.py
@@ -1,65 +1,8 @@
-import typing
-from collections.abc import Iterable, Mapping, Sequence
-from typing import TypeAlias, TypeVar, Union, overload
-
-T = TypeVar("T")
-Nested: TypeAlias = T | Mapping[str, "Nested[T]"] | Iterable["Nested[T]"]
+from __future__ import annotations
 
 
-def foo(x: float) -> int:
-    return 42
+def foo(x: int) -> int:
+    return x + 1
 
 
-# fmt: off
-@overload
-def sum_recursive(x: int) -> int: ...
-@overload
-def sum_recursive(x: Mapping[str, Nested[int]]) -> int: ...
-@overload
-def sum_recursive(x: Iterable[Nested[int]]) -> int: ...
-# fmt: on
-def sum_recursive(x):
-    total: int = 0
-    reveal_type(x)
-    match x:
-        case int() as number:
-            total += number
-        case Mapping() as mapping:
-            reveal_type(mapping)
-            for key in mapping:
-                total += sum_recursive(mapping[key])  # error
-        case Iterable() as iterable if not isinstance(iterable, Mapping):
-            for item in iterable:
-                total += sum_recursive(item)
-
-    return total
-
-
-#
-# Union[
-#     Mapping[
-#         str,
-#         Union[
-#             T @ Nested,
-#             Mapping[str, Nested],
-#             Iterable[Nested],
-#         ],
-#     ],
-#     Mapping[
-#         Union[
-#             int,
-#             Mapping[str, T @ Nested | Mapping[str, Nested] | Iterable[Nested]],
-#             Iterable[T @ Nested | Mapping[str, Nested] | Iterable[Nested]],
-#         ],
-#         Unknown,
-#     ],
-# ]
-
-
-#
-# Mapping[str, T @ Nested | Mapping[str, Nested] | Iterable[Nested]] | Mapping[
-#     Tensor
-#     | Mapping[str, T @ Nested | Mapping[str, Nested] | Iterable[Nested]]
-#     | Iterable[T @ Nested | Mapping[str, Nested] | Iterable[Nested]],
-#     Unknown,
-# ]
+print(foo.__annotations__)
