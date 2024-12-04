54cfcf04bdc89e0cfec1350789e57859da569c63
Randolf Scholz <randolf.scholz@gmail.com>
2024-12-04 20:18:58 +0100

    removed old run scripts

--- run/find_triple_quotes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# search for triple quotes that are not raw.
-regex='(?<=[^r])"""(?=[^\n\s])'
-echo "$regex"
--- run/pydeps.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# execute pydeps and create dependency graph
-pydeps src/linodenet/ --cluster --rankdir BT --max-bacon=1
