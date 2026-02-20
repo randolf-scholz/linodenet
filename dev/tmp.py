3cc03e8c75cc8ac711a47c218f0fa23ce086c9be
Randolf Scholz <randolf.scholz@gmail.com>
2026-02-20 15:49:21 +0100

    updated deps

--- dev/tmp.py
+++ dev/tmp.py
@@ -1,14 +1,35 @@
-from typing import reveal_type
+from typing import TypedDict, assert_never, assert_type
 
 
-class Foo[T]:
-    def __init__(self) -> None:
-        pass
+class D(TypedDict):
+    name: str
+    value: int
 
 
-class Bar[S](Foo[S]):
-    def __init__(self) -> None:
-        reveal_type(Foo.__init__)
-        reveal_type(Foo[S].__init__)
-        reveal_type(Foo[int].__init__)  # E: BoundMethod
-        Foo[S].__init__(self)
+def test_match_key(x: D | int) -> None:
+    match x:
+        case {"name": _}:
+            # equivalent to (isinstance(x, Mapping) and "name" in x)
+            assert_type(x, D)
+        case _:
+            assert_type(x, int)
+
+
+def test_match_key_and_value(x: D | int) -> None:
+    match x:
+        case {"name": str()}:
+            # equivalent to (isinstance(x, Mapping)
+            # and "name" in x and isinstance(x["name"], str))
+            assert_type(x, D)
+        case _:
+            assert_type(x, int)
+
+
+def test_non_match(x: D | int) -> None:
+    match x:
+        case {"value": str()}:
+            # equivalent to (isinstance(x, Mapping)
+            # and "value" in x and isinstance(x["value"], str))
+            assert_never(x)
+        case _:
+            assert_type(x, D | int)
