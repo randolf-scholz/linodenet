71635c7098800f400c8e76a094066ad6550d3632
Randolf Scholz <randolf.scholz@gmail.com>
2026-03-17 17:26:48 +0100

    Add rank_one and tridiagonal regularization functions; update __init__.py and modules.py to include new classes and exports.

--- src/linodenet/projections/__init__.py
+++ src/linodenet/projections/__init__.py
@@ -4,6 +4,7 @@ Notes:
     - See `linodenet.projections.functional` for functional implementations.
     - See `linodenet.projections.modules` for module-based implementations.
 """
+# ruff: noqa: F403, F405
 
 __all__ = [
     # Sub-Modules
@@ -13,6 +14,7 @@ __all__ = [
     # Constants
     "FUNCTIONAL_PROJECTIONS",
     "MODULAR_PROJECTIONS",
+    "SPECIAL_PROJECTIONS",
     "PROJECTIONS",
 ]
 
@@ -27,26 +29,30 @@ __all__ += surjections.__all__
 
 
 FUNCTIONAL_PROJECTIONS: dict[str, FunctionalProjection] = {
-    "banded"              : functional.banded,
-    "contraction"         : functional.contraction,
-    "diagonal"            : functional.diagonal,
-    "diagonally_dominant" : functional.diagonally_dominant,
-    "hamiltonian"         : functional.hamiltonian,
-    "identity"            : functional.identity,
-    "low_rank"            : functional.low_rank,
-    "lower_triangular"    : functional.lower_triangular,
-    "masked"              : functional.masked,
-    "normal"              : functional.normal,
-    "orthogonal"          : functional.orthogonal,
-    "rank_one"            : functional.rank_one,
-    "skew_symmetric"      : functional.skew_symmetric,
-    "symmetric"           : functional.symmetric,
-    "symplectic"          : functional.symplectic,
-    "traceless"           : functional.traceless,
-    "tridiagonal"         : functional.tridiagonal,
-    "upper_triangular"    : functional.upper_triangular,
+    "contraction"       : functional.contraction,
+    "diagonal"          : functional.diagonal,
+    "hamiltonian"       : functional.hamiltonian,
+    "identity"          : functional.identity,
+    "lower_triangular"  : functional.lower_triangular,
+    "normal"            : functional.normal,
+    "orthogonal"        : functional.orthogonal,
+    "rank_one"          : functional.rank_one,
+    "skew_symmetric"    : functional.skew_symmetric,
+    "symmetric"         : functional.symmetric,
+    "symplectic"        : functional.symplectic,
+    "traceless"         : functional.traceless,
+    "tridiagonal"       : functional.tridiagonal,
+    "upper_triangular"  : functional.upper_triangular,
 }  # fmt: skip
 r"""Dictionary of all available modular metrics."""
+
+SPECIAL_PROJECTIONS = {
+    "diagonally_dominant" : functional.diagonally_dominant,
+    "low_rank"            : functional.low_rank,
+    "banded"              : functional.banded,
+    "masked"              : functional.masked,
+}  # fmt: skip
+r"""Projections that require additional arguments"""
 
 MODULAR_PROJECTIONS: dict[str, type[ProjectionBase]] = {
     "Banded"             : modules.Banded,
