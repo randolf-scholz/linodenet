c498b7ecb9c0d363fc3a6f8d8ab16a48cb25dcaa
Randolf Scholz <randolf.scholz@gmail.com>
2026-03-17 18:00:37 +0100

    Update test_regularizations.py to use REGULARIZATIONS for parameterization and remove unused imports and tests.

--- tests/linodenet/regularizations/test_regularizations.py
+++ tests/linodenet/regularizations/test_regularizations.py
@@ -2,18 +2,11 @@ r"""Tests for linodenet.regularizations."""
 
 import pytest
 
-from linodenet.regularizations import (
-    FUNCTIONAL_REGULARIZATIONS,
-    MODULAR_REGULARIZATIONS,
-)
+from linodenet.regularizations import REGULARIZATIONS
 from tests.testing import camel2snake, snake2camel
 
 
-@pytest.mark.parametrize("projection_name", MODULAR_REGULARIZATIONS)
+@pytest.mark.parametrize("projection_name", REGULARIZATIONS)
 def test_modular(projection_name: str) -> None:
-    assert camel2snake(projection_name) in FUNCTIONAL_REGULARIZATIONS
-
-
-@pytest.mark.parametrize("projection_name", FUNCTIONAL_REGULARIZATIONS)
-def test_functional(projection_name: str) -> None:
-    assert snake2camel(projection_name) in MODULAR_REGULARIZATIONS
+    assert camel2snake(projection_name) in REGULARIZATIONS
+    assert snake2camel(projection_name) in REGULARIZATIONS
