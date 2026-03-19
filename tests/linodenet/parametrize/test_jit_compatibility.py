ea6e659a799a7a4b9dfe0dffe7900c84adc288e1
Randolf Scholz <randolf.scholz@gmail.com>
2026-03-19 13:37:32 +0100

    Refactor test_jit_compatibility.py: remove unused imports and functions, add new tests for model parameterization and serialization

--- tests/linodenet/parametrize/test_jit_compatibility.py
+++ tests/linodenet/parametrize/test_jit_compatibility.py
@@ -1,36 +1,12 @@
-from copy import deepcopy
+import tempfile
 
-import pytest
 import torch
-from torch import Tensor, nn
+from torch import Tensor, jit, nn
+from torch._dynamo import OptimizedModule
 from torch.nn.functional import mse_loss
 from torch.optim import SGD
 
-from linodenet.nn.parametrize import (
-    WrappedParametrization,
-    get_parametrizations,
-    is_parametrization,
-    parametrized,
-    register_parametrization,
-    update_parametrizations,
-)
-from linodenet.testing import (
-    all_close,
-    assert_model_ok,
-    check_jit_scriptable,
-    check_jit_serializable,
-    is_symmetric,
-    is_upper_triangular,
-)
-from linodenet.testing.utils import get_norm
-
-
-class Symmetric(nn.Module):
-    def forward(self, x: Tensor) -> Tensor:
-        return (x + x.transpose(-1, -2)) / 2
-
-    def right_inverse(self, y: Tensor) -> Tensor:
-        return y
+from linodenet.nn.parametrize import register_parametrization, update_parametrizations
 
 
 class UpperTriangular(nn.Module):
@@ -41,226 +17,82 @@ class UpperTriangular(nn.Module):
         return y
 
 
-def check_optimization(
-    model: nn.Module, *, args: tuple[Tensor, ...], target: Tensor
-) -> None:
+def is_upper_triangular(x: Tensor) -> bool:
+    return torch.equal(x, x.triu())
+
+
+def deserialize_module(module: nn.Module | jit.ScriptModule) -> jit.ScriptModule:
+    scripted = jit.script(module)
+
+    with tempfile.TemporaryFile() as file:
+        jit.save(scripted, file)
+        file.seek(0)
+        return jit.load(file)
+
+
+def assert_update_refreshes_parametrized_weight(model: nn.Module) -> None:
+    model.train()
+    inputs = torch.randn(3, 5)
+    target = torch.randn(3, 4)
     optimizer = SGD(model.parameters(), lr=0.1)
 
-    with torch.no_grad():
-        original_weights = [w.clone().detach() for w in model.parameters()]
-        original_outputs = model(*args)
-        original_loss = mse_loss(original_outputs, target)
-        original_params = [w.clone().detach() for w in model.parameters()]
-
-    # crucial, otherwise no update in the first iteration!
-    update_parametrizations(model)
-
-    # perform 1 training step
-    model.zero_grad(set_to_none=True)
-    outputs = model(*args)
-    loss = get_norm(outputs)
-    loss.backward()
-    assert all(w.grad is not None for w in model.parameters() if w.requires_grad)
-    assert loss.isfinite()
-    optimizer.step()
     update_parametrizations(model)
     assert isinstance(model.weight, Tensor)
+    weight_before = model.weight.detach().clone()
+    outputs_before = model(inputs).detach().clone()
+    params_before = [parameter.detach().clone() for parameter in model.parameters()]
+
+    model.zero_grad(set_to_none=True)
+    loss = mse_loss(model(inputs), target)
+    loss.backward()
+    optimizer.step()
+
+    weight_after_step = model.weight.detach().clone()
+    assert (
+        torch.equal(weight_after_step, weight_before) or weight_after_step.isnan().all()
+    )
+
+    update_parametrizations(model)
+
     assert is_upper_triangular(model.weight)
-    assert not all_close(original_weights, list(model.parameters()))
-
-    # check that the loss has decreased
-    assert loss < original_loss
-    # check that the outputs are different
-    new_outputs = model(*args)
-    assert not torch.allclose(new_outputs, original_outputs)
-    # check that the parameters are different
-    for x, y in zip(model.parameters(), original_params, strict=True):
-        assert not torch.allclose(x, y)
+    assert not torch.equal(model.weight, weight_before)
+    assert not torch.allclose(model(inputs), outputs_before)
+    assert any(
+        not torch.equal(parameter.detach(), before)
+        for parameter, before in zip(model.parameters(), params_before, strict=True)
+    )
 
 
-def test_jit_trainable_manual() -> None:
-    torch.manual_seed(42)
-    batch_size, dim_in, dim_out = 3, 5, 4
+def test_register_parametrization_uses_upper_triangular_cache() -> None:
+    model = nn.Linear(in_features=5, out_features=4, bias=False)
 
-    x = torch.randn(batch_size, dim_in)
-    y = torch.randn(batch_size, dim_out)
-    model = nn.Linear(in_features=dim_in, out_features=dim_out, bias=False)
-    original_weight = model.weight
-
-    check_jit_scriptable(model)
-
-    # create the parametrization
-    param = parametrized(model.weight, UpperTriangular())
-    assert param.original_parameter is model.weight
-    assert param.cached_parameter is not model.weight
-
-    # check that the parametrization is initialized
     assert not is_upper_triangular(model.weight)
-    assert not is_upper_triangular(param.original_parameter)
-    assert is_upper_triangular(param.cached_parameter)
-
-    # remove weight and re-register it as a buffer
-    delattr(model, "weight")
-    model.register_buffer("weight", param.cached_parameter)
-    model.register_module("param", param)
-    model.register_parameter("original_weight", param.original_parameter)
-    assert isinstance(model.param, WrappedParametrization)
-
-    # check that the parametrization is registered
-    assert model.weight is not original_weight
-    assert model.weight is model.param.cached_parameter
-    assert original_weight is model.param.original_parameter
-
-    # check that model can be scripted
-    check_jit_scriptable(model)
-
-    # initialize the parametrization
-    model.param.update_parametrization()
-
-    # verify that the parametrization is initialized
+    register_parametrization(model, "weight", UpperTriangular())
     assert is_upper_triangular(model.weight)
-    assert isinstance(model.param.original_parameter, Tensor)
-    assert is_upper_triangular(model.param.original_parameter)
-    assert isinstance(model.param.cached_parameter, Tensor)
-    assert is_upper_triangular(model.param.cached_parameter)
-
-    # test training plain model
-    check_optimization(model, args=(x,), target=y)
-
-    # test training scripted model
-    scripted = check_jit_scriptable(model)
-    check_optimization(scripted, args=(x,), target=y)
-
-    # test training serialized model
-    loaded = check_jit_serializable(scripted)
-    check_optimization(loaded, args=(x,), target=y)
 
 
-def test_jit_trainable() -> None:
-    r"""Tests the optimization of a JIT-compiled model."""
-    torch.manual_seed(42)
-    batch_size, dim_in, dim_out = 3, 5, 4
-    x = torch.randn(batch_size, dim_in)
-    y = torch.randn(batch_size, dim_out)
+def test_scripted_model_refreshes_parametrized_weight_during_training() -> None:
+    model = nn.Linear(in_features=5, out_features=4, bias=False)
+    register_parametrization(model, "weight", UpperTriangular())
+    compiled = jit.script(model)
+    assert is_upper_triangular(compiled.weight)
+    assert_update_refreshes_parametrized_weight(compiled)
 
-    model = nn.Linear(in_features=dim_in, out_features=dim_out, bias=False)
+
+def test_deserialized_model_refreshes_parametrized_weight_during_training() -> None:
+    model = nn.Linear(in_features=5, out_features=4, bias=False)
+    register_parametrization(model, "weight", UpperTriangular())
+    compiled = deserialize_module(model)
+    assert is_upper_triangular(compiled.weight)
+    assert_update_refreshes_parametrized_weight(compiled)
+
+
+def test_compile_compatibility() -> None:
+    model = nn.Linear(in_features=5, out_features=4, bias=False)
     register_parametrization(model, "weight", UpperTriangular())
 
-    scripted_model = check_jit_scriptable(model)
-    assert is_upper_triangular(scripted_model.weight)
-    check_optimization(scripted_model, args=(x,), target=y)
+    compiled = torch.compile(model)
 
-    deserialized_model = check_jit_serializable(model)
-    assert is_upper_triangular(deserialized_model.weight)
-    check_optimization(deserialized_model, args=(x,), target=y)
-
-
-def test_jit_preserves_parameters() -> None:
-    dim_in, dim_out = 2, 3
-    model = nn.Linear(in_features=dim_in, out_features=dim_out, bias=False)
-
-    original_parameters = deepcopy(tuple(model.parameters()))
-
-    deserialized_model = check_jit_serializable(model)
-    deserialized_param = deepcopy(tuple(deserialized_model.parameters()))
-
-    # apply only a dummy parametrization for this test.
-    register_parametrization(model, "weight", nn.Identity())
-    parametrized_parameters = deepcopy(tuple(model.parameters()))
-
-    deserialized_parametrized_model = check_jit_serializable(deserialized_model)
-    deserialized_parametrized_param = deepcopy(
-        tuple(deserialized_parametrized_model.parameters())
-    )
-
-    for x, y in zip(original_parameters, deserialized_param, strict=True):
-        assert x.shape == y.shape
-        assert torch.equal(x, y)
-
-    for x, y in zip(original_parameters, parametrized_parameters, strict=True):
-        assert x.shape == y.shape
-        assert torch.equal(x, y)
-
-    for x, y in zip(original_parameters, deserialized_parametrized_param, strict=True):
-        assert x.shape == y.shape
-        assert torch.equal(x, y)
-
-
-@pytest.mark.xfail(
-    reason="After deserialization update_parametrization must be called."
-)
-def test_jit() -> None:
-    r"""Test that subclasses of Protocol-class work with JIT."""
-    torch.manual_seed(42)
-
-    batch_size, dim_in, dim_out = 7, 5, 4
-    inputs = torch.randn(batch_size, dim_in)
-    model = nn.Linear(in_features=dim_in, out_features=dim_out, bias=False)
-
-    # check_object(model, input_args=(inputs,))
-
-    register_parametrization(model, "weight", UpperTriangular())
-
-    assert_model_ok(model, call_args=(inputs,))
-
-
-def test_jit_attribute() -> None:
-    dim_in, dim_out = 3, 5
-    model = nn.Linear(in_features=dim_in, out_features=dim_out, bias=False)
-    assert not is_upper_triangular(model.weight)
-
-    register_parametrization(model, "weight", UpperTriangular())
-    ps = get_parametrizations(model)
-    assert is_upper_triangular(model.weight)
-    assert is_parametrization(ps["weight"])
-
-    # check that model can be scripted
-    scripted = check_jit_scriptable(model)
-    scripted_ps = get_parametrizations(model)
-    assert is_upper_triangular(scripted.weight)
-    assert is_parametrization(scripted_ps["weight"])
-
-    # check that model can be scripted
-    deserialized_model = check_jit_serializable(model)
-    assert is_upper_triangular(deserialized_model.weight)
-    # assert is_parametrization(deserialized_model["weight"])
-
-
-def test_parametrized() -> None:
-    torch.manual_seed(42)
-    batch_size, dim_in, dim_out = 4, 3, 3
-
-    x = torch.randn(batch_size, dim_in)
-
-    # setup reference model
-    reference_model = nn.Linear(dim_in, dim_out, bias=False)
-    symmetric = Symmetric()
-    symmetrized_weight = symmetric(reference_model.weight)
-    reference_model.weight = nn.Parameter(symmetrized_weight)
-    assert is_symmetric(reference_model.weight)
-
-    # setup vanilla model
-    model = nn.Linear(dim_in, dim_out, bias=False)
-    with torch.no_grad():
-        model.weight.copy_(reference_model.weight)
-
-    # check compatibility
-    assert_model_ok(
-        model,
-        call_args=(x,),
-        call_kwargs={},
-        reference_model=reference_model,
-        test_jit=True,
-    )
-
-    # now, parametrizations
-    weight = model.weight
-    param = parametrized(weight, symmetric)
-    param.zero_grad(set_to_none=True)
-    model.weight = param.original_parameter
-    model.param = param
-
-    # check compatibility
-    assert_model_ok(
-        model, call_args=(x,), reference_model=reference_model, test_jit=True
-    )
+    assert isinstance(compiled, OptimizedModule)
+    assert is_upper_triangular(compiled.weight)
+    assert_update_refreshes_parametrized_weight(compiled)
