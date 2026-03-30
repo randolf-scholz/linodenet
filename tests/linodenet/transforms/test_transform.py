d9340454cd51919afaec8afa49ef69b906b10ca2
Randolf Scholz <randolf.scholz@gmail.com>
2026-03-30 20:10:25 +0200

    Add invertibility assertions with log absolute determinants to TestTransform

--- tests/linodenet/transforms/test_transform.py
+++ tests/linodenet/transforms/test_transform.py
@@ -7,10 +7,11 @@ from tests.testing import TestSuite
 
 
 class TestTransform(TestSuite):
-    def assert_reversible(
+    def assert_right_invertible(
         self,
         transform: Transform,
         arg: Tensor,
+        *,
         atol: float,
         rtol: float,
     ) -> None:
@@ -18,6 +19,64 @@ class TestTransform(TestSuite):
         decoded = transform.decode(encoded)
         self.assert_close(decoded, arg, atol=atol, rtol=rtol)
 
+    def assert_left_invertible(
+        self,
+        transform: Transform,
+        arg: Tensor,
+        *,
+        atol: float,
+        rtol: float,
+    ) -> None:
         decoded = transform.decode(arg)
         encoded = transform.encode(decoded)
         self.assert_close(encoded, arg, atol=atol, rtol=rtol)
+
+    def assert_right_invertible_with_logabsdet(
+        self,
+        transform: Transform,
+        arg: Tensor,
+        *,
+        atol: float,
+        rtol: float,
+    ) -> None:
+        encoded, forward_logabsdet = transform.encode_and_logabsdet(arg)
+        decoded, inverse_logabsdet = transform.decode_and_logabsdet(encoded)
+        self.assert_close(decoded, arg, atol=atol, rtol=rtol)
+        self.assert_close(
+            forward_logabsdet + inverse_logabsdet,
+            forward_logabsdet.new_zeros(forward_logabsdet.shape),
+            atol=atol,
+            rtol=rtol,
+        )
+
+    def assert_left_invertible_with_logabsdet(
+        self,
+        transform: Transform,
+        arg: Tensor,
+        *,
+        atol: float,
+        rtol: float,
+    ) -> None:
+        decoded, inverse_logabsdet = transform.decode_and_logabsdet(arg)
+        encoded, forward_logabsdet = transform.encode_and_logabsdet(decoded)
+        self.assert_close(encoded, arg, atol=atol, rtol=rtol)
+        self.assert_close(
+            forward_logabsdet + inverse_logabsdet,
+            forward_logabsdet.new_zeros(forward_logabsdet.shape),
+            atol=atol,
+            rtol=rtol,
+        )
+
+    def assert_invertible(
+        self,
+        transform: Transform,
+        x: Tensor,
+        y: Tensor,
+        *,
+        atol: float,
+        rtol: float,
+    ) -> None:
+        self.assert_right_invertible(transform, x, atol=atol, rtol=rtol)
+        self.assert_left_invertible(transform, y, atol=atol, rtol=rtol)
+        self.assert_right_invertible_with_logabsdet(transform, x, atol=atol, rtol=rtol)
+        self.assert_left_invertible_with_logabsdet(transform, y, atol=atol, rtol=rtol)
