6069d64b87d276617d6b3c54ae9a49f73ad85051
Randolf Scholz <randolf.scholz@gmail.com>
2026-06-24 10:48:26 +0200

    Rename separable_flows.py to mnf.py; update __init__.py to include new model classes CRU, GRU_D, GRU_ODE_Bayes, and Grafiti.

--- src/linodenet/forecasting/__init__.py
+++ src/linodenet/forecasting/__init__.py
@@ -7,21 +7,31 @@ __all__ = [
     "PointForecastingModel",
     "ProbabilisticForecastingModel",
     # Classes
-    "LatentStateSpaceModel",
-    "LinODEnet",
+    "CRU",
     "ContinuousKalmanFilter",
     "DiscreteKalmanFilter",
+    "GRU_D",
+    "GRU_ODE_Bayes",
+    "Grafiti",
     "LastValue",
+    "LatentStateSpaceModel",
+    "LinODEnet",
+    "MarginalizableNormalizingFlow",
     "ProFITi",
     "Shiesh",
 ]
 
 from .base import PointForecastingModel, ProbabilisticForecastingModel
 from .continuous_kalman_filter import ContinuousKalmanFilter
+from .cru import CRU
 from .discrete_kalman_filter import DiscreteKalmanFilter
+from .grafiti import Grafiti
+from .gru_d import GRU_D
+from .gru_ode_bayes import GRU_ODE_Bayes
 from .last_value import LastValue
 from .linodenet import LinODEnet
 from .lssm import LatentStateSpaceModel
+from .mnf import MarginalizableNormalizingFlow
 from .profiti import ProFITi, Shiesh
 
 FORECASTING_MODELS: dict[str, type[PointForecastingModel]] = {}
