02d2cf6249482d72d500de10647194277ea32772
Randolf Scholz <randolf.scholz@gmail.com>
2026-07-13 16:33:31 +0200

    Add ConjugateAffineFlow to the list of scalar transforms in __init__.py

--- src/linodenet/mappings/__init__.py
+++ src/linodenet/mappings/__init__.py
@@ -93,6 +93,7 @@ TRANSFORMS: dict[str, type[Transform]] = {
     "TransformSequence"    : base.TransformSequence,
     "TriangularTransform"  : transforms.TriangularTransform,
     # scalar transforms
+    "ConjugateAffineFlow"  : transforms.scalar.ConjugatedAffineFlow,
     "CELU"                 : transforms.scalar.CELU,
     "Exp"                  : transforms.scalar.Exp,
     "ELU"                  : transforms.scalar.ELU,
