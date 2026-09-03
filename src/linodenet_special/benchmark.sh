176447c6b71ad1ffd1f7860634c895b116e17ae8
Randolf Scholz <randolf.scholz@gmail.com>
2026-09-03 11:12:52 +0200

    Remove benchmark.sh, build.sh, and compare.sh scripts; add Justfile for build and test automation

--- src/linodenet_special/benchmark.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-pytest tests/linodenet_special/test_performance.py::test_spectral_norm_forward \
-  -n 0 --no-cov \
-  --benchmark-group-by="func,param:device,param:shape" \
-  --benchmark-save=base \
-  --benchmark-sort=mean \
-  --benchmark-compare \
--- src/linodenet_special/compare.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-pytest-benchmark compare --histogram .benchmarks/histograms/ --group-by="func,param:device,param:shape" --name "trial"
